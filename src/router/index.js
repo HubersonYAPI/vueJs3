f73b8d7fe5ca3165f5179e9792c7df48f98b3e85
Huberson Yapi <hubersonyapi@yahoo.fr>
2021-05-27 15:21:35 +0000

    creation de model

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/tut1',
+    name: 'tut1',
+    // route level code-splitting
+    // this generates a separate chunk (tut1.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "tut1" */ '../views/pages/tut1.vue')
   }
 ]
 
---
+++ src/views/pages/tut1.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal" >
+      <template v-slot:links>
+        <a href="#">S'inscrire</a>
+        <a href="#">Plus d'info</a>
+      </template>
+      <h1>Acheter à moindre coût</h1>
+      <p>offrez-vous ce merveilleur article à moitié prix</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo" >
+      <h1>S'abonner à la newsletter</h1>
+      <p>Bénéficier des dernières mises à jour et de codes promos</p>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal" >Ouvrir le Model (alt)</button>
+  <button @click="toggleModalTwo" >Ouvrir le Model</button>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+  name: "tut1",
+  components: {Modal},
+  data(){
+    return {
+      title: "Mon Premier Projet Vue Js",
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+
+  methods:{
+    toggleModal() {
+      this.showModal = !this.showModal 
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo 
+    }
+  }
+  
+};
+</script>
