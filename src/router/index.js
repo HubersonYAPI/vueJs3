0b7f4aae0a1389bf3b72f2fdb4376f6584516fb0
Huberson Yapi <hubersonyapi@yahoo.fr>
2021-05-28 03:21:08 +0000

    initialisation du projet Gestion de musiques

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Music from '../views/audio/Music.vue'
+
 
 const routes = [
   {
@@ -29,6 +31,12 @@ const routes = [
     // this generates a separate chunk (form.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "form" */ '../views/pages/Form.vue')
+  },
+  {
+    path: '/music',
+    name: 'Music',
+    component: Music
+
   },
   {
     path: '/reaction',
---
+++ src/views/audio/Music.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Projet Audio</h1>
+</template>
+
+<script>
+
+    export default {
+    name:'Music'
+    }
+</script>
+
+<style>
+
+</style>
